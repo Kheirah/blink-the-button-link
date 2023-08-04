fa947d32b3774b4361ad217a5ef61b4ca7ecf35a
Ernst Stolz <eric@spiritsdontfly.com>
2023-08-04 14:15:55 +0200

    add bunnies and carrots

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -16,99 +16,103 @@ export default function Home() {
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
         <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
+          <p>This is Blink</p>
         </div>
 
         <div className={styles.center}>
           <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
+            src="/blink.png"
+            alt="Blink"
+            width={640}
+            height={640}
             priority
+            style={{ borderRadius: "10px" }}
           />
         </div>
+        <div className={styles.description}>
+          <p>Blink is a Bunny and a Link.</p>
+        </div>
+        <div className={styles.description}>
+          <p>
+            He's the protector of web development law and living symbol of
+            proper web element usage.
+          </p>
+        </div>
+        <div className={styles.description}>
+          <p>
+            Every day he hops through the World Wide Web and keeps an eye on how
+            developers use links and buttons.
+          </p>
+        </div>
 
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
+        <div className={styles.center}>
+          <Image
+            src="/female_developer.png"
+            alt="female developer"
+            width={640}
+            height={640}
+            priority
+            style={{ borderRadius: "10px" }}
+          />
+        </div>
+        <div className={styles.description}>
+          <p>
+            Whenever a young developer is about to build a link that looks like
+            a button, Blink is there to help.
+          </p>
+        </div>
+        <div className={styles.description}>
+          <p>
+            Often he sees links dressed as buttons with attributes and styles
+            they shouldn't have.
+          </p>
+        </div>
+        <div className={styles.description}>
+          <p>
+            It's as if a bunny were pretending to be a T-Rex, a confusing sight!
+          </p>
+        </div>
 
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
+        <div className={styles.center}>
+          <Image
+            src="/blink.png"
+            alt="Blink"
+            width={640}
+            height={640}
+            priority
+            style={{ borderRadius: "10px" }}
+          />
+        </div>
+        <div className={styles.description}>
+          <p>
+            Filled with wisdom gathered from his travels through HTML and CSS,
+            Blink tells the developer never to confuse links and buttons.
+          </p>
+        </div>
+        <div className={styles.description}>
+          <p>
+            To this day, Blink continues his journey, hopping and linking
+            through the web, leaving behind a trail of well-coded websites.
+          </p>
+        </div>
 
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
+        <div className={styles.center}>
+          <Image
+            src="/hugging.png"
+            alt="developer and blink hugging"
+            width={640}
+            height={640}
+            priority
+            style={{ borderRadius: "10px" }}
+          />
+        </div>
+        <div className={styles.description}>
+          <p>
+            The next time you see a link or a button, remember Blink, the cute
+            little protector of the web.
+          </p>
         </div>
       </main>
     </>
-  )
+  );
 }
